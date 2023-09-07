69951a8197cbac4e359808968337c352e9585b82
mephist-cne <mephist.siu@cne.com.hk>
2023-09-07 14:39:00 +0800

    UWP 1.0.2.2
    1. remove pfx file
    2. set title to "CS710S RFID Reader App"
    3. CSLibrary rename to CSLibraryBT5

--- MobileMvxApp/BLE.Client.UWP/Properties/AssemblyInfo.cs
+++ MobileMvxApp/BLE.Client.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BLE.Client.UWP")]
+[assembly: AssemblyTitle("CS710SCSDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BLE.Client.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("CSL")]
+[assembly: AssemblyProduct("CS710S RFID Reader App")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.2")]
+[assembly: AssemblyFileVersion("1.0.2.2")]
 [assembly: ComVisible(false)]
